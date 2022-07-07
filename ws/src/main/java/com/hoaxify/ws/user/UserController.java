e9becdc78376a7d242051fa3fee3836a90f7d13b
BurakBebek1 <burakbebek1994@gmail.com>
2022-07-07 14:13:38 +0300

    User Signup

--- ws/src/main/java/com/hoaxify/ws/user/UserController.java
+++ ws/src/main/java/com/hoaxify/ws/user/UserController.java
@@ -1,7 +1,10 @@
 package com.hoaxify.ws.user;
 
+import com.hoaxify.ws.shared.GenericResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,8 +13,13 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserController {
     private static final Logger log = LoggerFactory.getLogger(UserController.class);
 
+    @Autowired
+    UserService userService;
+
     @PostMapping("/api/1.0/users")
-    public void CreateUser(@RequestBody User user) {
-        log.info(user.toString());
+    public GenericResponse CreateUser(@RequestBody User user) {
+
+        userService.save(user);
+        return new GenericResponse("User Created");
     }
 }
