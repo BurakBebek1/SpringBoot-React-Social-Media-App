38c92d151b28bf45057748207df5b3f009d0e63a
BurakBebek1 <burakbebek1994@gmail.com>
2022-07-22 14:37:33 +0300

    User Login 2

--- ws/src/main/java/com/hoaxify/ws/user/User.java
+++ ws/src/main/java/com/hoaxify/ws/user/User.java
@@ -1,5 +1,7 @@
 package com.hoaxify.ws.user;
 
+import com.fasterxml.jackson.annotation.JsonView;
+import com.hoaxify.ws.shared.Views;
 import lombok.Data;
 
 import javax.persistence.Entity;
@@ -22,15 +24,21 @@ public class User {
     @NotNull(message = "{hoaxify.constraint.username.NotNull.message}")
     @Size(min = 4, max = 255)
     @UniqueUsername
+    @JsonView(Views.Base.class)
     private String username;
 
     @NotNull
     @Size(min = 4, max = 255)
+    @JsonView(Views.Base.class)
     private String displayName;
 
     @NotNull
     @Size(min = 8, max = 255)
     @Pattern(regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d).*$", message = "{hoaxify.constraint.password.Pattern.message}")
+    @JsonView(Views.Sensitive.class)
     private String password;
+
+    @JsonView(Views.Base.class)
+    private String image;
 
 }
